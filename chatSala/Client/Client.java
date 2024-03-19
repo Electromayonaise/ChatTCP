00df2b027685276570431a47a40df1d25e9d721c
jul109 <115905294+jul109@users.noreply.github.com>
2024-03-19 08:15:04 -0500

    Update Client.java

--- chatSala/Client/Client.java
+++ chatSala/Client/Client.java
@@ -6,19 +6,38 @@ public class Client {
     private static final int PORT = 6789;
 
     public static void main(String[] args) {
+        DataInputStream in; //del servidor al cliente
+        DataOutputStream out; //del cliente al servidor
+        
         try {
             Socket socket = new Socket(SERVER_IP, PORT);
             System.out.println("Conectado al servidor.");
 
             String message;
             //canal de entrada para el usuario
+            
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in)); 
             
             //usando el socket, crear los canales de entrada in y salida out
+             in =new DataInputStream(socket.getInputStream());
+            out =new DataOutputStream(socket.getOutputStream());
+
+            do {
+                    //solicitar al usuario un alias, o nombre y enviarlo al servidor
+                System.out.println("Escriba el nombre de usuario");
+                
+                //no debe salir de este bloque hasta que el nombre no sea aceptado
+                //al ser aceptado notificar, de lo contrario seguir pidiendo un alias
+                out.writeUTF(userInput.readLine());
+    
+                
+            } while (in.readUTF()=="El nombre de usuario ya existe");
+
+            
                       
-            //solicitar al usuario un alias, o nombre y enviarlo al servidor
-            //no debe salir de este bloque hasta que el nombre no sea aceptado
-            //al ser aceptado notificar, de lo contrario seguir pidiendo un alias
+            
+            
+
 
 
                  
@@ -35,3 +54,4 @@ public class Client {
         }
     }
 }
+
