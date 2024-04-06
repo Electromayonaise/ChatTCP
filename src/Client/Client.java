de4d14d0ab11bbe39f86244dd9b628001b4927a5
jul109 <115905294+jul109@users.noreply.github.com>
2024-04-05 22:05:41 -0500

    reset

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -4,12 +4,12 @@ import java.net.*;
 public class Client {
     private static final String SERVER_IP = "localhost";
     private static final int PORT = 6789;
-    private static final AudioRecorderPlayer recorder=new AudioRecorderPlayer();
+    
 
     public static void main(String[] args) {
         BufferedReader in; //del servidor al cliente
         PrintWriter out; //del cliente al servidor
-        recorder.record();
+        
         
         try {
             Socket socket = new Socket(SERVER_IP, PORT);
